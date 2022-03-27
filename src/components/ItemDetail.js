9598b943bde5767d4766e37d8da503d714a50e6c
Jonatan Prudencio <jona.prudencio14@hotmail.com>
2022-03-27 20:50:49 -0300

    Proyecto Final .2

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import ItemCount from "./ItemCount";
-import { useContext, useEffect , useState } from "react";
+import iconCamion from '../images/icon-camion.png';
+import iconDev from '../images/icon-dev.png';
 
+import iconQuestion from '../images/question.png';
+import { useContext, useEffect , useState } from "react";
+import swal from "sweetalert";
 import { CartContext } from "./CartContext";
 
 
@@ -14,7 +18,7 @@ export default function ItemDetail({item}){
     const test = useContext(CartContext);
     
     const onAdd = (qty) =>{
-        alert("Se agregaron " + qty + " items");
+        swal("Se agregaron " + qty + " items");
         setItemCount(qty);
         test.addItem(item, qty);
     }
@@ -47,26 +51,108 @@ export default function ItemDetail({item}){
                <option>XL</option>
                 </select>
 
+                {
+
+             
+itemCount === 0
+? <ItemCount stock={item.stock} initial={itemCount} onAdd={onAdd} 
+/>
+: <Link to='/cart' style={{textDecoration: "none"}}><button className="quantityAdd-button">checkout</button></Link>
+     }
+
+                
+                <div className="product-shipping">
+           <div className="btn btn--cfg" data-bs-toggle="modal" data-bs-target="#exampleModal">
+
+           <div className="product-shipping__free">
+   
+               <div className="product-shipping__free-icon">
+   
+                   <img src={iconCamion} alt="" className="product-shipping__icon"></img>
+               </div>
+   
+               <div className="product-shipping__free-info">
+   
+                   <div className="product-shipping__free-info-title">
+                       Envío Gratuito
+                       <img src={iconQuestion} alt="" className="product-shipping__free-info__title-icon"></img>
+                   </div> 
+                   <div className="product-intro__freeshipping-desc"><span>Envío normal gratuito para pedidos de más de $5.000</span>
+   
+                   </div>
+   
+               </div>
+   
+           </div>
+   
+   
+   
+     </div>
+
+     <div type="button" className="btn btn--cfg" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+     <div className="product-shipping__dev">
+
+                   <div className="product-shipping__dev-icon">
+                       <img src={iconDev} alt="" className="product-shipping__icon"></img>
+                   </div>
+
+                   <div><div className="product-shipping__dev-title">
+                       Devoluciones
+                       <img src={iconQuestion} alt="" className="product-shipping__dev-title__icon"></img>
+                   </div> <div className="product-intro__freeshipping-desc">
+                       Más información
+                   </div></div>
+
+                   
+
+               </div>
+  
+    </div>
+
+               
+           </div>
+
+       
+
                 <div className="product-des">
 
                <h4 className="mt-5 mb-5 font--7">Descripción del producto</h4>
 
-           <span>{item.descripcion}</span>
+               <div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingOne">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Material y Detalles
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingTwo">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Envíos, Cambios y Devoluciones
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+          
 
            </div>
 
-          
-          
-             {
+           
 
-             
-            itemCount === 0
-            ? <ItemCount stock={item.stock} initial={itemCount} onAdd={onAdd} 
-            />
-            : <Link to='/cart' style={{textDecoration: "none"}}><button className="quantityAdd-button">checkout</button></Link>
-                 }
-        
-    
+          
          
 
             
